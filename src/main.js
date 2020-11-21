dd95a9beb6d21a618bb289584c132b797c101061
tyort <neotyo1986@gmail.com>
2020-11-21 20:12:48 +0300

    Генерируем данные для компонента формы

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {createMenuTemplate} from "./view/menu.js";
 import {createSortTemplate} from "./view/sorting.js";
 import {createAllMoviesTemplate} from "./view/movies-all.js";
 import {createMovieCardTemplate} from "./view/movie-card.js";
+import {createMovieEditTemplate} from "./view/movie-edit.js";
 import {createShowMoreTemplate} from "./view/show-more.js";
 import {generateCard} from "./mock/card.js";
 
@@ -26,6 +27,10 @@ const filmsLists = films.querySelectorAll(`.films-list`);
 filmsLists.forEach((list, index) => {
   const container = list.querySelector(`.films-list__container`);
   const count = index === 0 ? COMMON_CARD_COUNT : EXTRA_CARD_COUNT;
+
+  if (index === 0) {
+    render(container, createMovieEditTemplate(cards[0]));
+  }
 
   for (let i = 0; i < count; i++) {
     render(container, createMovieCardTemplate(cards[i]));
