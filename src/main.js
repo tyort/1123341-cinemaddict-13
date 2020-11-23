a938cbc046fb7ce5fd0d9cc316d1ebc480cf12d2
tyort <neotyo1986@gmail.com>
2020-11-23 23:34:43 +0300

    Продолжаем описывать логику допоказа

--- src/main.js
+++ src/main.js
@@ -29,28 +29,37 @@ const filmsLists = films.querySelectorAll(`.films-list`);
 
 filmsLists.forEach((list, index) => {
   const container = list.querySelector(`.films-list__container`);
-  const count = index === 0 ? COMMON_CARD_COUNT : EXTRA_CARD_COUNT;
+  const count = index === 0 ? CARD_COUNT_STEP : EXTRA_CARD_COUNT;
 
   // if (index === 0) {
   //   render(container, createMovieEditTemplate(cards[0]));
   // }
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < Math.min(cards.length, count); i++) {
     render(container, createMovieCardTemplate(cards[i]));
   }
 });
 
 if (cards.length > CARD_COUNT_STEP) {
+  let renderedCardsCount = CARD_COUNT_STEP;
   render(filmsLists[0], createShowMoreTemplate());
   const showMoreButton = siteMainElement.querySelector(`.films-list__show-more`);
 
   showMoreButton.addEventListener(`click`, (evt) => {
     evt.preventDefault();
-    alert(`Works!`);
+    const container = films.querySelector(`.films-list`).querySelector(`.films-list__container`);
+    cards
+      .slice(renderedCardsCount, renderedCardsCount + CARD_COUNT_STEP)
+      .forEach((card) => render(container, createMovieCardTemplate(card)));
+
+    renderedCardsCount += CARD_COUNT_STEP;
+
+    if (renderedCardsCount >= cards.length) {
+      showMoreButton.remove();
+    }
   });
 }
 
 function render(container, template, place = `beforeend`) {
   container.insertAdjacentHTML(place, template);
 }
-
