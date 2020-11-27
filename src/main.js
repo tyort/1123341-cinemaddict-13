9c973bcd9fb19ea915f46df38f0eb611ac57e3dd
tyort <neotyo1986@gmail.com>
2020-11-27 13:02:43 +0300

    Рефакторинг рендер-метода

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {createMovieCardTemplate} from "./view/movie-card.js";
 import {createShowMoreTemplate} from "./view/show-more.js";
 import {generateCard} from "./mock/card.js";
 import {generateFilter} from "./mock/filter.js";
+import {renderTemplate} from "./utils.js";
 
 const EXTRA_CARD_COUNT = 2;
 const COMMON_CARD_COUNT = 22;
@@ -19,10 +20,10 @@ const body = document.querySelector(`body`);
 const siteHeaderElement = body.querySelector(`.header`);
 const siteMainElement = body.querySelector(`.main`);
 
-render(siteHeaderElement, createUserRankTemplate());
-render(siteMainElement, createMenuTemplate(filters));
-render(siteMainElement, createSortTemplate());
-render(siteMainElement, createAllMoviesTemplate());
+renderTemplate(siteHeaderElement, createUserRankTemplate());
+renderTemplate(siteMainElement, createMenuTemplate(filters));
+renderTemplate(siteMainElement, createSortTemplate());
+renderTemplate(siteMainElement, createAllMoviesTemplate());
 
 const films = siteMainElement.querySelector(`.films`);
 const filmsLists = films.querySelectorAll(`.films-list`);
@@ -32,17 +33,17 @@ filmsLists.forEach((list, index) => {
   const count = index === 0 ? CARD_COUNT_STEP : EXTRA_CARD_COUNT;
 
   // if (index === 0) {
-  //   render(container, createMovieEditTemplate(cards[0]));
+  //   renderTemplate(container, createMovieEditTemplate(cards[0]));
   // }
 
   for (let i = 0; i < Math.min(cards.length, count); i++) {
-    render(container, createMovieCardTemplate(cards[i]));
+    renderTemplate(container, createMovieCardTemplate(cards[i]));
   }
 });
 
 if (cards.length > CARD_COUNT_STEP) {
   let renderedCardsCount = CARD_COUNT_STEP;
-  render(filmsLists[0], createShowMoreTemplate());
+  renderTemplate(filmsLists[0], createShowMoreTemplate());
   const showMoreButton = siteMainElement.querySelector(`.films-list__show-more`);
 
   showMoreButton.addEventListener(`click`, (evt) => {
@@ -50,7 +51,7 @@ if (cards.length > CARD_COUNT_STEP) {
     const container = films.querySelector(`.films-list`).querySelector(`.films-list__container`);
     cards
       .slice(renderedCardsCount, renderedCardsCount + CARD_COUNT_STEP)
-      .forEach((card) => render(container, createMovieCardTemplate(card)));
+      .forEach((card) => renderTemplate(container, createMovieCardTemplate(card)));
 
     renderedCardsCount += CARD_COUNT_STEP;
 
@@ -60,6 +61,3 @@ if (cards.length > CARD_COUNT_STEP) {
   });
 }
 
-function render(container, template, place = `beforeend`) {
-  container.insertAdjacentHTML(place, template);
-}
