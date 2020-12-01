cd7a75b8fd3b44007370b1966552946bcc789770
tyort <neotyo1986@gmail.com>
2020-12-02 00:37:57 +0300

    Выносим общие методы в абстрактный класс

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -1,7 +1,27 @@
+import {createElement} from "../utils.js";
+
 export default class Abstract {
   constructor() {
     if (new.target === Abstract) {
       throw new Error(`Can't instantiate Abstract, only concrete one.`);
     }
+
+    this._element = null;
+  }
+
+  getTemplate() { // метод создает строковый шаблон, а ниже превращает в DOM-элемент
+    throw new Error(`Abstract method not implemented: getTemplate`);
+  }
+
+  getElement() { // метод превращает в DOM-элемент полученную строку сверху
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
