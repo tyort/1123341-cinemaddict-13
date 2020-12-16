70f363029c8ef2144e47c3bae7b4cdc2ac3719cd
tyort <neotyo1986@gmail.com>
2020-12-16 14:46:10 +0300

    Рефакторинг

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -1,23 +1,28 @@
 import Abstract from "./abstract.js";
 import {SortType} from "../const.js";
 
-const createSortTemplate = () => {
+const createSortTemplate = (checkedSortType) => {
+  const isDefaultActive = SortType.DEFAULT === checkedSortType ? `sort__button--active` : ``;
+  const isDateActive = SortType.DATE === checkedSortType ? `sort__button--active` : ``;
+  const isRateActive = SortType.RATING === checkedSortType ? `sort__button--active` : ``;
+
   return `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
-    <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
-    <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
+    <li><a href="#" class="sort__button ${isDefaultActive}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button ${isDateActive}" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button ${isRateActive}" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`;
 };
 
 export default class Sort extends Abstract {
-  constructor() {
+  constructor(checkedSortType) {
     super();
+    this._checkedSortType = checkedSortType;
     this._handler = {};
     this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
   }
 
   getTemplate() {
-    return createSortTemplate();
+    return createSortTemplate(this._checkedSortType);
   }
 
   _sortTypeChangeHandler(evt) {
