9217d64ff9c58fe0ed9b01cb4088c060cb53a16c
tyort <neotyo1986@gmail.com>
2020-12-20 02:23:27 +0300

    Добавляем компонент статистики

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import UserRank from "./view/user-rank.js";
 import Menu from "./view/menu.js";
+import Statistics from "./view/statistics.js";
 import InnerMain from "./presenter/main-inside.js";
 import FilterPresenter from "./presenter/filter.js";
 import CardsModel from "./model/cards.js";
@@ -13,24 +14,22 @@ const COMMON_CARD_COUNT = 22;
 const body = document.querySelector(`body`);
 const siteHeaderElement = body.querySelector(`.header`);
 const siteMainElement = body.querySelector(`.main`);
-
-const cards = new Array(COMMON_CARD_COUNT).fill().map(generateCard);
-
-const cardsModel = new CardsModel();
-cardsModel.setCards(cards); // возвращается копия cards в переменную this._cards. Ничего не возвращаем!
-
-const filterModel = new FilterModel();
-
-const innerMainPresenter = new InnerMain(siteMainElement, filterModel, cardsModel);
-
 render(siteHeaderElement, new UserRank());
 const menu = new Menu();
 render(siteMainElement, menu);
-const mainNavigation = siteMainElement.querySelector(`.main-navigation`);
+const filterModel = new FilterModel();
 
-const filterPresenter = new FilterPresenter(mainNavigation, filterModel, cardsModel);
-
+const cards = new Array(COMMON_CARD_COUNT).fill().map(generateCard);
+const cardsModel = new CardsModel();
+cardsModel.setCards(cards); // возвращается копия cards в переменную this._cards. Ничего не возвращаем!
+const innerMainPresenter = new InnerMain(siteMainElement, filterModel, cardsModel);
 innerMainPresenter.renderInnerMain();
+
+const statistics = new Statistics(cardsModel.getCards());
+render(siteMainElement, statistics);
+
+const mainNavigation = siteMainElement.querySelector(`.main-navigation`);
+const filterPresenter = new FilterPresenter(mainNavigation, filterModel, cardsModel);
 filterPresenter.init();
 
 const handleSiteMenuClick = (menuItem) => {
