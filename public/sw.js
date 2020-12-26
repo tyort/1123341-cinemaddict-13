ddf7a300a03c3b56d39f6a237a7c9d782eb90ce3
tyort <neotyo1986@gmail.com>
2020-12-27 02:08:22 +0300

    рефакторинг

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_PREFIX = `taskmanager-cache`;
+const CACHE_PREFIX = `cinemaddict-cache`;
 const CACHE_VER = `v13`;
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
 
@@ -6,25 +6,39 @@ const HTTP_STATUS_OK = 200;
 const RESPONSE_SAFE_TYPE = `basic`;
 
 self.addEventListener(`install`, (evt) => {
-  evt.waitUntil(
-      caches.open(CACHE_NAME)
+  evt.waitUntil( // sw не установится пока не выполнится содержимое метода
+      caches.open(CACHE_NAME) // открытие хранилища
         .then((cache) => {
           return cache.addAll([
             `/`,
             `/index.html`,
             `/bundle.js`,
             `/css/normalize.css`,
-            `/css/style.css`,
-            `/fonts/HelveticaNeueCyr-Bold.woff`,
-            `/fonts/HelveticaNeueCyr-Bold.woff2`,
-            `/fonts/HelveticaNeueCyr-Medium.woff`,
-            `/fonts/HelveticaNeueCyr-Medium.woff2`,
-            `/fonts/HelveticaNeueCyr-Roman.woff`,
-            `/fonts/HelveticaNeueCyr-Roman.woff2`,
-            `/img/add-photo.svg`,
-            `/img/close.svg`,
-            `/img/sample-img.jpg`,
-            `/img/wave.svg`,
+            `/css/main.css`,
+            `/fonts/OpenSans-Bold.woff2`,
+            `/fonts/OpenSans-ExtraBold.woff2`,
+            `/fonts/OpenSans-Regular.woff2`,
+            `/images/background.png`,
+            `/images/bitmap.png`,
+            `/images/bitmap@2x.png`,
+            `/images/bitmap@3x.png`,
+            `/images/emoji/angry.png`,
+            `/images/emoji/puke.png`,
+            `/images/emoji/sleeping.png`,
+            `/images/emoji/smile.png`,
+            `/images/icons/icon-favorite-active.svg`,
+            `/images/icons/icon-favorite.svg`,
+            `/images/icons/icon-watched-active.svg`,
+            `/images/icons/icon-watched.svg`,
+            `/images/icons/icon-watchlist-active.svg`,
+            `/images/icons/icon-watchlist.svg`,
+            `/images/posters/made-for-each-other.png`,
+            `/images/posters/popeye-meets-sinbad.png`,
+            `/images/posters/sagebrush-trail.jpg`,
+            `/images/posters/santa-claus-conquers-the-martians.jpg`,
+            `/images/posters/the-dance-of-life.jpg`,
+            `/images/posters/the-great-flamarion.jpg`,
+            `/images/posters/the-man-with-the-golden-arm.jpg`
           ]);
         })
   );
